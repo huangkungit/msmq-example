70917b4d94a700c7f9ca73c74541401a6fa8402c
huangkungit <kunhuang_uestc@163.com>
2015-08-27 10:51:56 +0800

    Update IDeclareProxyAPI.cs

--- CustomsDeclarationProxy/Hessian/IDeclareProxyAPI.cs
+++ CustomsDeclarationProxy/Hessian/IDeclareProxyAPI.cs
@@ -7,10 +7,7 @@ namespace CustomsDeclarationProxy.Hessian
 {
     public interface IDeclareProxyAPI
     {
-        //place : 1(海关)，2（园区政府）
         Boolean sendDeclMessage(string messageId, string outId, int sendType, string messageDetail, int place);
-
         String getResponseMessageByMessageId(string messageId, int type, int place);
-
     }
 }
